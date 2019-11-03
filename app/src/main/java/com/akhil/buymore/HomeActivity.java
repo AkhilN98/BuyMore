7961239b18cf3448a519482dfe81013db026998b
Akhil <akhil.techgeek@gmail.com>
2019-11-02 23:11:32 -0700

    Added functionality to Home Screen * After logging In user can now view items posted by admin

--- app/src/main/java/com/akhil/buymore/HomeActivity.java
+++ app/src/main/java/com/akhil/buymore/HomeActivity.java
@@ -1,14 +1,74 @@
 package com.akhil.buymore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.akhil.buymore.Model.Products;
+import com.akhil.buymore.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 public class HomeActivity extends AppCompatActivity {
+    private DatabaseReference ProdectRef;
+    private RecyclerView recyclerview;
+    RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ProdectRef = FirebaseDatabase.getInstance().getReference().child("Products");
+
+
+        recyclerview = findViewById(R.id.recycler_menu);
+        recyclerview.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerview.setLayoutManager(layoutManager);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Products> options =
+                new FirebaseRecyclerOptions.Builder<Products>()
+                .setQuery(ProdectRef, Products.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull Products model) {
+                        holder.txtProductName.setText(model.getPname());
+                        holder.txtProductDescription.setText(model.getDescription());
+                        holder.txtProductPrice.setText("Price= $ " + model.getPrice());
+                        Picasso.get().load(model.getImage()).into(holder.imageView);
+                    }
+
+                    @NonNull
+                    @Override
+                    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_items_layout,parent,false);
+                        ProductViewHolder holder = new ProductViewHolder(view);
+                        return holder;
+                    }
+                };
+
+        recyclerview.setAdapter(adapter);
+        adapter.startListening();
     }
 }
